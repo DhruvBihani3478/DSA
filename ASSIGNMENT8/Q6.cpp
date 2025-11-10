07dd8a180cbdf2768c9fb3c57721d8c638161dc3
Dhruv Bihani <dhruvbihani3478@gmail.com>
2025-11-10 10:21:54 +0530

    codes

--- ASSIGNMENT8/Q6.cpp
+++ ASSIGNMENT8/Q6.cpp
@@ -2,7 +2,7 @@
 #include<stack>
 #include<queue>
 using namespace std;
-int helper(priority_queue<int, vector<int>, greater<int> >pq    , int k){
+int helper(priority_queue<int, vector<int>, greater<int> >pq, int k){
     stack<int>s;
     int temp;
     for(int i=0;i<k-1;i++){
@@ -10,22 +10,17 @@ int helper(priority_queue<int, vector<int>, greater<int> >pq    , int k){
         s.push(temp);
         pq.pop();
     }
-    
     return pq.top();
 }
 int main(){
-    priority_queue<int, vector<int>, greater<int> > pq;
+    priority_queue<int, vector<int>, greater<int>> pq;
     pq.push(-10);
     pq.push(20);
     pq.push(50);
     pq.push(-45);
     pq.push(45);
     pq.push(80);
-    cout<<helper(pq,1)<<endl;
-    cout<<helper(pq,2)<<endl;
-    cout<<helper(pq,3)<<endl;
-    cout<<helper(pq,4)<<endl;
-    cout<<helper(pq,5)<<endl;
-    cout<<helper(pq,6)<<endl;
-
+    for (int k = 1; k <= pq.size(); k++)
+        cout<< helper(pq, k) << endl;
 }
+
